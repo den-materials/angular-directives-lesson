82758782aa37e1c291833d91c8c90d1ad563a8ec
Ben Hulan <benhulan@gmail.com>
2016-07-05 11:25:50 -0700

    removes personal API key

--- solution-code/app.js
+++ solution-code/app.js
@@ -1,6 +1,5 @@
-var app = angular.module('newApp', []);
-
-app.directive('currentWeather', function(){
+angular.module('newApp', [])
+    .directive('currentWeather', function(){
         return {
             restrict: 'E',
             scope: {
@@ -9,7 +8,7 @@ app.directive('currentWeather', function(){
             template: '<div class="current-weather"><h4>Weather for {{ city }}</h4>{{weather.main.temp}} degrees farenheit</div>',
             controller: ['$scope', '$http', function($scope, $http){
                 var url="http://api.openweathermap.org/data/2.5/weather?mode=json&cnt=7&units=imperial&callback=JSON_CALLBACK&q=";
-                var apikey = "&appid=44d52dfe2dfd9e96e7fd20d53e579dd2";
+                var apikey = "&appid=";  // add your own API key here
                 $scope.getWeather = function(city){
                     $http({method: 'JSONP', url: url + city + apikey})
                         .success(function(data){
